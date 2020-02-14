5ac2d505ee6a382bc0a77f9a1d9e11de7984fbfa
katakatataan <yuki.matsuyama0123@gmail.com>
2020-02-14 19:37:35 +0900

    説明を追加

---
+++ delete.sh
@@ -0,0 +1,121 @@
+#!/usr/bin/env bash
+# set -x
+source env.sh
+
+export PROJECT_NAME
+export KEY_PAIR_NAME
+export ENVIORNMENT_NAME
+export PROFILE
+export APP_S3_BUCKET
+export APP_S3_KEY
+export SERVICE_NAME
+export PRIVATE_DOMAIN_NAME
+export DOMAIN_NAME
+export DB_PRIVATE_DOMAIN_NAME
+export CLOUDFORMATION_TEMPLATE_S3_BUCKET
+export CODEPIPELINE_ARTIFACT_S3_BACKET_NAME
+
+
+echo '------------------------------------------------------------------------------------------'
+echo 'delete s3 bucket for cloudformation yml'
+echo '------------------------------------------------------------------------------------------'
+if ! aws s3 ls "s3://$CLOUDFORMATION_TEMPLATE_S3_BUCKET" --profile $PROFILE --region ap-northeast-1 2>&1 | grep -q 'An error occurred'; then
+  echo "bucketName: $CLOUDFORMATION_TEMPLATE_S3_BUCKET"
+  aws s3 rb s3://$CLOUDFORMATION_TEMPLATE_S3_BUCKET \
+    --region ap-northeast-1 \
+    --profile $PROFILE \
+    --force
+  aws s3api wait bucket-not-exists \
+    --region ap-northeast-1 \
+    --profile $PROFILE \
+    --bucket $CLOUDFORMATION_TEMPLATE_S3_BUCKET
+fi
+
+echo '------------------------------------------------------------------------------------------'
+echo 'delete s3 bucket for elasticbeanstalk app source yml'
+echo '------------------------------------------------------------------------------------------'
+if aws s3 ls "s3://$APP_S3_BUCKET" --profile $PROFILE --region ap-northeast-1 2>&1 | grep -q 'An error occurred'; then
+  aws s3 rb s3://$APP_S3_BUCKET \
+    --region ap-northeast-1 \
+    --profile $PROFILE \
+    --force
+  aws s3api wait bucket-not-exists \
+    --region ap-northeast-1 \
+    --profile $PROFILE \
+    --bucket $APP_S3_BUCKET
+fi
+
+echo '------------------------------------------------------------------------------------------'
+echo 'delete ecr bucket'
+echo '------------------------------------------------------------------------------------------'
+echo 'should delete ecr repository manually'
+echo 'https://ap-northeast-1.console.aws.amazon.com/ecr/repositories?region=ap-northeast-1'
+
+echo '------------------------------------------------------------------------------------------'
+echo 'delete api stack'
+echo '------------------------------------------------------------------------------------------'
+if ! aws cloudformation describe-stacks --profile $PROFILE --region ap-northeast-1 --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-${SERVICE_NAME} 2>&1 1>/dev/null | grep error; then
+  stackStatus=$(aws cloudformation describe-stacks --profile $PROFILE --region ap-northeast-1 --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-${SERVICE_NAME}| jq -r ".Stacks[0].StackStatus")
+  echo "stackStatus: $stackStatus"
+  echo "stackName: ${PROJECT_NAME}-${ENVIORNMENT_NAME}-${SERVICE_NAME} wait for delete"
+  aws cloudformation delete-stack \
+    --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-${SERVICE_NAME} \
+    --region ap-northeast-1 \
+    --profile $PROFILE
+  aws cloudformation wait stack-delete-complete \
+    --profile $PROFILE \
+    --region ap-northeast-1 \
+    --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-${SERVICE_NAME}
+fi
+
+echo '------------------------------------------------------------------------------------------'
+echo 'delete db stack'
+echo '------------------------------------------------------------------------------------------'
+if ! aws cloudformation describe-stacks --profile $PROFILE --region ap-northeast-1 --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-db 2>&1 1>/dev/null | grep error; then
+  stackStatus=$(aws cloudformation describe-stacks --profile $PROFILE --region ap-northeast-1 --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-db| jq -r ".Stacks[0].StackStatus")
+  echo "stackStatus: $stackStatus"
+  echo "stackName: ${PROJECT_NAME}-${ENVIORNMENT_NAME}-db wait for delete"
+  aws cloudformation delete-stack \
+    --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-db \
+    --region ap-northeast-1 \
+    --profile $PROFILE
+  aws cloudformation wait stack-delete-complete \
+    --profile $PROFILE \
+    --region ap-northeast-1 \
+    --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-db
+fi
+
+echo '------------------------------------------------------------------------------------------'
+echo 'delete dns stack'
+echo '------------------------------------------------------------------------------------------'
+if ! aws cloudformation describe-stacks --profile $PROFILE --region ap-northeast-1 --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-dns 2>&1 1>/dev/null | grep error; then
+  stackStatus=$(aws cloudformation describe-stacks --profile $PROFILE --region ap-northeast-1 --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-dns| jq -r ".Stacks[0].StackStatus")
+  echo "stackStatus: $stackStatus"
+  echo "stackName: ${PROJECT_NAME}-${ENVIORNMENT_NAME}-dns wait for delete"
+  aws cloudformation delete-stack \
+    --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-dns \
+    --region ap-northeast-1 \
+    --profile $PROFILE
+  aws cloudformation wait stack-delete-complete \
+    --profile $PROFILE \
+    --region ap-northeast-1 \
+    --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-dns
+fi
+
+echo '------------------------------------------------------------------------------------------'
+echo 'delete vpc stack'
+echo '------------------------------------------------------------------------------------------'
+if ! aws cloudformation describe-stacks --profile $PROFILE --region ap-northeast-1 --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-vpc 2>&1 1>/dev/null | grep error; then
+  stackStatus=$(aws cloudformation describe-stacks --profile $PROFILE --region ap-northeast-1 --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-vpc| jq -r ".Stacks[0].StackStatus")
+  echo "stackStatus: $stackStatus"
+  echo "stackName: ${PROJECT_NAME}-${ENVIORNMENT_NAME}-vpc wait for delete"
+  aws cloudformation delete-stack \
+    --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-vpc \
+    --region ap-northeast-1 \
+    --profile $PROFILE
+  aws cloudformation wait stack-delete-complete \
+    --profile $PROFILE \
+    --region ap-northeast-1 \
+    --stack-name ${PROJECT_NAME}-${ENVIORNMENT_NAME}-vpc
+fi
+
--- env.sh
+++ env.sh
@@ -1,14 +1,24 @@
+# 変更しても良い:自分の任意のプロジェクト名に変更
 export PROJECT_NAME=cloudformation-handson
+# 要変更: 自分で作成したec2 keypair名に変更
 export KEY_PAIR_NAME=cloudformation-handson-matsuyay
+# 変更しても良い: ただしdevかprod しか選択できない。
 export ENVIORNMENT_NAME=dev
+# aws configure --profile プロファイル名 のプロファイル名に指定したものに変更
 export PROFILE=cloudhandson
-# s3 は全世界で一意のかぶらないもの
+# 要変更:s3 は全世界で一意のかぶらないものに変更
 export APP_S3_BUCKET=app-s3-bucket-cloudformation-hands-on-api
+# 変更しない: elasticbeanstalk の一番初めの起動に使われる静的ファイルが格納されてる。
 export APP_S3_KEY=sample.zip
+# 要変更:s3 は全世界で一意のかぶらないものに変更
 export CODEPIPELINE_ARTIFACT_S3_BACKET_NAME=cloudformationhandson-codepipeline
+# 要変更:s3 は全世界で一意のかぶらないものに変更
 export CLOUDFORMATION_TEMPLATE_S3_BUCKET=yyyyyyy-cloudformation-hands-on-resource
-
+# 変更しても良い: apiのstack名と関連するリソースが書き換わる。プライベートDNSも変わる。
 export SERVICE_NAME=api
+# 変更してもよい: privateなdomain名が書き換わる。今回はapiとdbのdomainが影響をうける
 export PRIVATE_DOMAIN_NAME=private-cloudformation-handson.com
+# 変更してもよい:ただ形式は {任意のsubdomain}.${PRIVATE_DOMAIN_NAME}の制約がある。
 export DB_PRIVATE_DOMAIN_NAME=db.${PRIVATE_DOMAIN_NAME}
+# 変更してもよい: 擬似的なpublicドメイン。ちゃんとしたものにするにはdomainをregistryから購入してawsのname serverを参照。
 export DOMAIN_NAME=public-cloudformation-handson.com
